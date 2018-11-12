57122ac80cbe64ff4934fc894157de604e5b8a36
Criptdestroyer <ahmademir1201@gmail.com>
2018-11-12 07:18:43 +0700

    login admin done

---
+++ e-admin/index.php
@@ -0,0 +1,94 @@
+<?php require_once("auth.php") ?>
+<!DOCTYPE html>
+<html lang="en">
+<?php include("partials/head.php") ?>
+  
+
+  <body id="page-top">
+
+    <?php include("partials/navbar.php") ?>
+
+    <div id="wrapper">
+
+      <?php include("partials/sidebar.php") ?>
+
+      <div id="content-wrapper">
+
+        <div class="container-fluid">
+
+          <!-- Breadcrumbs-->
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+              <a href="#">Dashboard</a>
+            </li>
+            <li class="breadcrumb-item active">Overview</li>
+          </ol>
+
+         
+          <!-- DataTables Example -->
+          <div class="card mb-3">
+            <div class="card-header">
+              <i class="fas fa-table"></i>
+              Data Table Example</div>
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                      <th>Office</th>
+                      <th>Age</th>
+                      <th>Start date</th>
+                      <th>Salary</th>
+                    </tr>
+                  </thead>
+                  <tfoot>
+                    <tr>
+                      <th>Name</th>
+                      <th>Position</th>
+                      <th>Office</th>
+                      <th>Age</th>
+                      <th>Start date</th>
+                      <th>Salary</th>
+                    </tr>
+                  </tfoot>
+                  <tbody>
+                    <tr>
+                      <td>Tiger Nixon</td>
+                      <td>System Architect</td>
+                      <td>Edinburgh</td>
+                      <td>61</td>
+                      <td>2011/04/25</td>
+                      <td>$320,800</td>
+                    </tr>
+                    
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+        <?php include("partials/footer.php") ?>
+
+      </div>
+      <!-- /.content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- Scroll to Top Button-->
+    <?php include("partials/scrolltop.php") ?>
+
+    <!-- Logout Modal-->
+    <?php include("partials/modal.php") ?>
+
+    <?php include("partials/js.php") ?>
+
+  </body>
+
+</html>
