5bc73a9c882a918b4cdff72314756977f476c2ea
adityoqrren <aji.adityo8@gmail.com>
2018-11-14 20:21:53 +0700

    hmmm

---
+++ ezuca/partials/logo.php
@@ -0,0 +1,55 @@
+<div class="clients-logo">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6">
+              <h5>Sponsor</h5>
+              <div class="row">
+                <div class="col-lg-4 col-md-4 logo-wrap">
+                  <a href="#">
+                    <img class="img-fluid" src="assets/baturaja.png" alt="">
+                  </a>
+                </div><!-- .logo-wrap-->
+
+                <div class="col-lg-4 col-md-4 logo-wrap">
+                  <a href="#">
+                    <img class="img-fluid "src="assets/codepolitan.png" alt="">
+                  </a>
+                </div><!-- .logo-wrap-->
+
+                <div class="col-lg-4 col-md-4 logo-wrap">
+                  <a href="#">
+                    <img class="img-fluid "src="assets/cyruscircle.png" alt="">
+                  </a>
+                </div><!-- .logo-wrap-->
+
+                <div class="col-lg-4 col-md-4 logo-wrap">
+                  <a href="#">
+                    <img class="img-fluid" src="assets/ELTI.png" alt="">
+                  </a>
+                </div><!-- .logo-wrap-->
+
+                <div class="col-lg-4 col-md-4 logo-wrap">
+                  <a href="#">
+                    <img class="img-fluid" src="assets/ideapromo.jpg" alt="">
+                  </a>
+                </div><!-- .logo-wrap-->
+
+                <div class="col-lg-4 col-md-4 logo-wrap">
+                  <a href="#">
+                    <img class="img-fluid" src="assets/jon project.jpg" alt="">
+                  </a>
+                </div><!-- .logo-wrap-->
+
+                <div class="col-lg-4 col-md-4 logo-wrap">
+                  <a href="#">
+                    <img class="img-fluid" src="assets/sriwijaya host.jpg" alt="">
+                  </a>
+                </div><!-- .logo-wrap-->
+              </div>
+
+            </div><!-- .col-->
+            <div class="col-lg-6 col-md-6">
+            </div>
+        </div><!-- .row-->
+    </div><!-- .container-->
+</div><!-- .clients-logo -->
---
+++ ezuca/single-page-lecture.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include("partials/head.php");?>
+<body class="single-courses-page">
+    <div class="page-header-lecture">
+        <?php include("partials/navlectures.php");?>
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12 lecturetitle">
+              <h1>TITLE</h1>
+              <h2>Lecture 1 : <span>Subtitle</span></h2>
+            </div>
+          </div>
+        </div>
+    </div><!-- .page-header -->
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-12">
+              Materi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
+              incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+               ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
+               in voluptate velit esse cillum dolore eu
+              fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+              deserunt mollit anim id est laborum.
+            </div><!-- .col -->
+            <div class="col-md-12 col-lg-12">
+              <form class="" action="#" method="post">
+                <h4>Question</h4>
+                <div class="row">
+                  <div class="col-md-12 col-lg-12 myquestion">
+                    <div class="number">No 1</div>
+                    yevbrbvbrbbrhbbvhrbhbrbthbrtbgbrgbvgbbbuhfvbhbfgh
+                    <div class="row">
+                      <!-- Pakai Looping -->
+                      <div class="col-md-3">
+                        <input type="radio" value="a" name="jawaban_no1">a. Anu</input>
+                      </div>
+                      <!-- <div class="col-md-3">
+                        b.Anu
+                      </div>
+                      <div class="col-md-3">
+                        c.Anu
+                      </div>
+                      <div class="col-md-3">
+                        d.Anu
+                      </div> -->
+                    </div>
+                  </div>
+                  <div class="col-md-12 col-lg-12 myquestion">
+                    No1
+                  </div>
+                  <div class="col-md-12 col-lg-12 myquestion">
+                    No1
+                  </div>
+                  <div class="col-md-12 col-lg-12 myquestion">
+                    No1
+                  </div>
+                  <div class="col-md-12 col-lg-12 myquestion">
+                    No1
+                  </div>
+              </form>
+
+              </div>
+            </div><!-- .col -->
+        </div><!-- .row -->
+    </div><!-- .container -->
+
+    <?php include("partials/logo.php") ?>
+
+  <?php include("partials/footer.php"); ?>
+
+    <script type='text/javascript' src='js/jquery.js'></script>
+    <script type='text/javascript' src='js/swiper.min.js'></script>
+    <script type='text/javascript' src='js/masonry.pkgd.min.js'></script>
+    <script type='text/javascript' src='js/jquery.collapsible.min.js'></script>
+    <script type='text/javascript' src='js/custom.js'></script>
+
+</body>
+</html>
